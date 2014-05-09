169d8d6da285270629e1da421058d29cf679450a
VioletaP <popova.vili@gmail.com>
2014-05-09 20:41:07 +0300

    two file.write-s: for BG and ENG

--- generate.rb
+++ generate.rb
@@ -26,6 +26,32 @@ File.write("./en/index.html",'<!DOCTYPE html>
 </body>
 </html>')
 
+File.write("./bg/index.html",'<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title> Violeta </title>
+	<link rel="stylesheet" type="text/css" href="../styles/style.css">
+	
+</head>
+<body>
+
+	<h1>%{headline}</h1>
+
+	
+	<article 
+		class="main"
+		title="Hello!"
+	>
+			<p id="first_line">%{firstline}</p>
+			<p id="second_line">%{secondline}</p><p>%{name}</p>
+			<a href="http://www.facebook.com/violeta.popova">Facebook</a> 
+			<a href="../en/index.html">ENG</a>
+			&lt;end&gt;
+	</article>
+
+</body>
+</html>')
 # en_text = {
 # :headline => "Zen spot",
 # :firstline => "LIVE as if you were to die tomorrow.",
