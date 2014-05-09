c6ebd10334472926d8de7ae7121551ca00c63a28
VioletaP <popova.vili@gmail.com>
2014-05-09 22:19:55 +0300

    extract html content into separate file
    and reference of the variable  in generate.rb to this file

--- generate.rb
+++ generate.rb
@@ -1,31 +1,5 @@
 #encoding: utf-8
-html_content = '
-	<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title> Violeta </title>
-	<link rel="stylesheet" type="text/css" href="../styles/style.css">
-	
-</head>
-<body>
-
-	<h1>%{headline}</h1>
-
-		<article 
-		class="main"
-		title="Hello!"
-	>
-			<p id="first_line">%{firstline}</p>
-			<p id="second_line">%{secondline}</p><p>%{name}</p>
-			<a href="http://www.facebook.com/violeta.popova">Facebook</a> 
-			<a href="../bg/index.html">BG</a>
-			<a href="../en/index.html">ENG</a>
-			&lt;end&gt;
-	</article>
-
-</body>
-</html>'
+html_content = File.read("./template.html")
 
 en_text = {
 :headline => "Zen spot",
