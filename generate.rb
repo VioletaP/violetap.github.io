77f7a188fb579cb67dc90e9a7a627137a1bd388b
VioletaP <popova.vili@gmail.com>
2014-05-09 21:44:35 +0300

    defined variable "html_content"
    reference to the existing hash

--- generate.rb
+++ generate.rb
@@ -1,5 +1,6 @@
 #encoding: utf-8
-File.write("./en/index.html",'<!DOCTYPE html>
+html_content = '
+	<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
@@ -11,8 +12,7 @@ File.write("./en/index.html",'<!DOCTYPE html>
 
 	<h1>%{headline}</h1>
 
-	
-	<article 
+		<article 
 		class="main"
 		title="Hello!"
 	>
@@ -20,48 +20,31 @@ File.write("./en/index.html",'<!DOCTYPE html>
 			<p id="second_line">%{secondline}</p><p>%{name}</p>
 			<a href="http://www.facebook.com/violeta.popova">Facebook</a> 
 			<a href="../bg/index.html">BG</a>
-			&lt;end&gt;
-	</article>
-
-</body>
-</html>')
-
-File.write("./bg/index.html",'<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title> Violeta </title>
-	<link rel="stylesheet" type="text/css" href="../styles/style.css">
-	
-</head>
-<body>
-
-	<h1>%{headline}</h1>
-
-	
-	<article 
-		class="main"
-		title="Hello!"
-	>
-			<p id="first_line">%{firstline}</p>
-			<p id="second_line">%{secondline}</p><p>%{name}</p>
-			<a href="http://www.facebook.com/violeta.popova">Facebook</a> 
 			<a href="../en/index.html">ENG</a>
 			&lt;end&gt;
 	</article>
 
 </body>
-</html>')
-# en_text = {
-# :headline => "Zen spot",
-# :firstline => "LIVE as if you were to die tomorrow.",
-# :secondline => "LEARN as if you were to live forever.",
-# :name => "Gandhi",
-# }
+</html>'
 
-# bg_text = {
-# :headline => "Дзен кътче",
-# :firstline => "ЖИВЕЙ така, сякаш няма да има утре.",
-# :secondline => "УЧИ така, сякаш ще живееш вечно...",
-# :name =>"Ганди",
-# }
+en_text = {
+:headline => "Zen spot",
+:firstline => "LIVE as if you were to die tomorrow.",
+:secondline => "LEARN as if you were to live forever.",
+:name => "Gandhi",
+}
+
+bg_text = {
+:headline => "Дзен кътче",
+:firstline => "ЖИВЕЙ така, сякаш няма да има утре.",
+:secondline => "УЧИ така, сякаш ще живееш вечно...",
+:name =>"Ганди",
+}
+
+File.write("./en/index.html", html_content % en_text)
+
+File.write("./bg/index.html", html_content % bg_text)
+
+
+
+#puts("Name:%{headline}%{firstline}".%(bg_text))
